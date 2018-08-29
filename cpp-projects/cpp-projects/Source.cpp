3f9275c32b91a6b9a628cd47594c2e76a0c49b10
manu-github <manumanuel@live.in>
2018-08-29 19:02:03 +0200

    Testing git account

---
+++ cpp-projects/cpp-projects/Source.cpp
@@ -0,0 +1,15 @@
+//Test github programme
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	string name;
+	cout << "Enter your name";
+	getline(cin, name);
+	cout << name;
+	return 0;
+}
