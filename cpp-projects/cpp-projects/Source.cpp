5838e04e8c75bb79039e20e3d897dfb3ee02ccc6
manu-github <manumanuel@live.in>
2018-08-29 20:23:17 +0200

    calculator

--- cpp-projects/cpp-projects/Source.cpp
+++ cpp-projects/cpp-projects/Source.cpp
@@ -1,41 +1,54 @@
-//calculator
+//calculator_edited with simplw functions
 
 #include<iostream>
 
 using namespace std;
 
+
+
+int add(int, int);
+int substract(int, int);
+double divide(int, int);
+int multiply(int, int);
+
 int main() {
 
 	char op = 'c';
 	int x, y;
-	while (op != 'e') {
+	while (1) {
 		cout<<"What operation would you like to perform: add(+),substract(-),divide(/),multiply(*),[e]xit?"<<endl;
 		cin >> op;
-		cout << "Enter the numbers to be operated";
-		switch (op) {
+		
+		switch (op) {			
 			case '+':
 				cin >> x >> y;
-				cout << x << " + " << y << " " << " = " << x + y<<endl;
-			
+				cout << x << " + " << y << " " << " = " << add(x,y)<<endl;
+				break;			
 			case '-':
 				cin >> x >> y;
-				cout << x << " - " << y << " " << " = " << x - y << endl;
+				cout << x << " - " << y << " " << " = " << substract(x, y) << endl;
 			case '*':
+				
 				cin >> x >> y;
-				cout << x << " * " << y << " " << " = " << x * y << endl;
-
+				cout << x << " * " << y << " " << " = " << multiply(x, y) << endl;
+				break;
 			case '/':
 				cin >> x >> y;
-				cout << x << " / " << y << " " << " = " << static_cast<double>(x) / y << endl;
-			
-			case 'e':
+				cout << x << " / " << y << " " << " = " << divide(x, y) << endl;
 				break;
+			case 'e':
+				return 0;
 			default:				
 				cout <<"Sorry,try again...!!!" << endl;
-
-		}
+				break;
+			}
 	}
 
 	system("pause");
 	return 0;
 }
+
+int add(int x, int y) { return x + y; }
+int substract(int x, int y) { return x - y; }
+double divide(int x, int y) { return static_cast<double>(x) / y; }
+int multiply(int x, int y) { return x * y; }
