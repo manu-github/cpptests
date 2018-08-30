0fb2b179c44e78ee7ec917a18eb4383cc0241d1c
manu-github <manumanuel@live.in>
2018-08-30 10:05:36 +0200

    permute

--- cpp-projects/cpp-projects/Source.cpp
+++ cpp-projects/cpp-projects/Source.cpp
@@ -1,85 +1,33 @@
-//Temperature Converter Challenge
-#include<iostream>
-#include<iomanip>
+//permuite---not fully by myself
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-const int MAX = 50000;
-const int MIN = 0;
-//function reads the inputs
-void input(int&, int&, int&);
-//function prints the inputs and other values
-void cvtTable(int&, int&, int&);
-//function convert the temperature
-double convertTemp(double);
+string switchchars(string, int, int);
+void permute(string, int);
 
-int main() {
-	
-	int start, end, step;
-	//input the values
-	input(start, end, step);
-	cvtTable(start, end, step);
+int main()
+{	
+	permute("manu", 0);
 	system("pause");
-	return 0;
 }
 
-
-void input(int& start, int& end, int& step){
-
-	//get the start temperature
-	cout << "Please give in a lower limit, limit >= 0:";
-	cin >> start;
-	while (start < MIN) {
-		cout << "Your start reange is too less, Try again :";
-		cin >> start;
-	}
-
-	//get the end temperature
-	cout << "Please give in a higher limit, 10 > limit <= 50000 :";
-	cin >> end;
-	while (end > MAX) {
-		cout << "Your start reange is too high, Try again :";
-		cin >> end;
-	}
-
-	//get the stepsize
-	cout << "Please give in a step, 0 < step <= 10: ";
-	cin >> step;
-	while (step > (end - start)) {
-		cout << "Your step size too high, Try again :";
-		cin >> step;
-	}
-
-
+string switchchars(string topermute, int x, int y)
+{
+	string newstring = topermute;
+	newstring[x] = newstring[y];
+	newstring[y] = topermute[x]; //avoids temp variable
+	return newstring;
 }
-
-
-void cvtTable(int& start, int& end, int& step) {
-
-	cout.width(20);
-	cout.setf(ios::left);
-	cout << "Celsius";
-	cout.width(20);
-	cout.setf(ios::left);
-	cout << "Fahrenheit"<<endl;
-	cout.width(20);
-	cout.setf(ios::left);
-	cout << "----------";
-	cout.width(20);
-	cout.setf(ios::left);
-	cout << "----------" << endl;
-
-	for (double i = start; i < end; i = i + step) {	
-
-		cout.width(20);
-		cout.setf(ios::left);
-		cout << fixed << setprecision(6) << i;
-		cout.width(20);
-		cout.setf(ios::left);
-		cout << fixed << setprecision(6) << convertTemp(i)<<endl;
+void permute(string topermute, int place)
+{
+	if (place == topermute.length() - 1)
+	{
+		cout << topermute << endl;
 	}
-}
-
-double convertTemp(double temp) {
-	return (temp * 9) / 5 + 32;	
+	for (int nextchar = place; nextchar < topermute.length(); nextchar++)
+	{
+		permute(switchchars(topermute, place, nextchar), place + 1);
+	}
 }
