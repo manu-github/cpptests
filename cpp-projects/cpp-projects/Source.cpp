7f21ac842b8dcf97dfe7967538266d4ee6566b63
manu-github <manumanuel@live.in>
2018-09-01 16:51:51 +0200

    normal factorial with recurcive function

--- cpp-projects/cpp-projects/Source.cpp
+++ cpp-projects/cpp-projects/Source.cpp
@@ -1,73 +1,57 @@
-//conversion failed commmit
 #include<iostream>
 #include<string>
-#include<vector>
 
 using namespace std;
+//get the number in the desired format
+void getNumber(int&);
+//finding the factorial
+int findFact(int);
+int checkZero(int);
 
-//formating the values less than thousand
-string thousand(int);
-//formatting the values greater than or equal to thousand
-string findNum(int);
-
-
-const string first[] = { "","one","two","three","four","five","six","seven","eight","nine","ten","elven","twelve","thirteen",
-																	"fourteen","fifteen","sixteen","seventeen","eighteen","nineteen" };
-const string second[] = { "","","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninty" };
-
-
-int main(int argc,char** argv) {
-	vector<int>numbers;
-	int num;
-	cout<<"enter the number : ";
-	cin>> num;
-	cout << findNum(num) <<endl;
-	cout << argc<<endl;
-	cout << argv[0]<<endl;
-	//system("pause");
+int main(int argc, char** argv) {
+	int number;
+	getNumber(number);
+	int fact = findFact(number);
+	cout << "number is " << fact << endl;
+	cout<<"Number of tailering zeros :"<< checkZero(fact)<<endl;
+	system("pause");
 	return 0;
 }
-string findNum(int temp) {
-	string outTemp1;
-	int num1, num2;
-	if (temp >= 1000000000) {
-		num1 = temp / 1000000000;
-		num2 = temp % 1000000000;
-		outTemp1 = outTemp1 +thousand(num1) + " " + "billion ";
-		outTemp1 = outTemp1 +findNum(num2);
+
+void getNumber(int& num) {
+	string temp;
+	while (1) {
+		cout << "Enter a number in the format (x-!): ";
+		cin >> num;
+		getline(cin,temp);
+		if (temp == "-!") {
+			break;
+		}
+		else {
+			cout << "Enter in the correct format"<<endl;
+		}
 	}
-	else if (temp >= 1000000) {
-		num1 = temp / 1000000;
-		num2 = temp % 1000000;
-		outTemp1 = outTemp1 +thousand(num1) + " " + "million ";
-		outTemp1 = outTemp1 +findNum(num2);
-	}
-	else if (temp >= 1000) {
-		num1 = temp / 1000;
-		num2 = temp % 1000;
-		outTemp1 = outTemp1 + thousand(num1) +" " + "thousand ";
-		outTemp1 = outTemp1 + thousand(num2);
-	}
-	else {
-		outTemp1 = thousand(temp);
-	}	
-	return outTemp1;
 }
 
-
-string thousand(int temp) {
-	string outTemp2;
-	if (temp >= 100&&temp>=20) {
-		outTemp2 =first[temp / 100] + " " + "hundred ";
-		temp = temp % 100;
+int findFact(int num) {
+	int fact=num;
+	if (num == 1) {
+		return fact;
 	}
-	if (temp < 20) {
-		outTemp2 = outTemp2 +first[temp];
-	}
-	else {
-		outTemp2 = outTemp2 + second[temp / 10] + " " + first[temp % 10];
-	}
-	return outTemp2;
+	fact = fact * findFact(num-1);
+	return fact; 
 
-
+}
+int checkZero(int temp) {
+	int zeros=0;
+	while(1) {		
+		if (temp % 10==0) {
+			temp = temp / 10;
+			zeros++;
+		}
+		else {
+			break;
+		}
+	}
+	return zeros;
 }
