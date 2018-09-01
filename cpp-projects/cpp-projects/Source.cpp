b9b9f2e3ed6b3e1215b83215bed0ba6e870efac6
manu-github <manumanuel@live.in>
2018-09-01 21:39:21 +0200

    Facorial of larger numbers

--- cpp-projects/cpp-projects/Source.cpp
+++ cpp-projects/cpp-projects/Source.cpp
@@ -1,57 +1,43 @@
 #include<iostream>
-#include<string>
-
 using namespace std;
-//get the number in the desired format
-void getNumber(int&);
-//finding the factorial
-int findFact(int);
-int checkZero(int);
+int multiply(int, int[],int);
 
-int main(int argc, char** argv) {
-	int number;
-	getNumber(number);
-	int fact = findFact(number);
-	cout << "number is " << fact << endl;
-	cout<<"Number of tailering zeros :"<< checkZero(fact)<<endl;
+#define MAX 500
+int main() {
+	int num,size=1;
+
+	int digits[MAX] = { 1 };
+	cout << "Enter the number: ";
+	cin >> num;
+	
+	//without recursion
+	for (int i = 2; i <= num; i++) {
+		size=multiply(i,digits,size);
+	}
+
+	cout << "Factoiral is :";
+	for (int k = size - 1; k >= 0; k--) {
+		cout << digits[k];
+	}
+	cout << endl;
 	system("pause");
 	return 0;
 }
 
-void getNumber(int& num) {
-	string temp;
-	while (1) {
-		cout << "Enter a number in the format (x-!): ";
-		cin >> num;
-		getline(cin,temp);
-		if (temp == "-!") {
-			break;
-		}
-		else {
-			cout << "Enter in the correct format"<<endl;
-		}
+int multiply(int num, int temp[],int size) {
+
+	int mul;
+	int carry = 0;
+	for (int j = 0; j < size; j++) {
+		mul = num * temp[j] + carry;
+		temp[j] = mul %10;
+		carry = mul / 10;
 	}
+	while (carry) {
+		temp[size] = carry % 10;
+		carry = carry / 10;
+		size++;
+	}
+	return size;
 }
 
-int findFact(int num) {
-	int fact=num;
-	if (num == 1) {
-		return fact;
-	}
-	fact = fact * findFact(num-1);
-	return fact; 
-
-}
-int checkZero(int temp) {
-	int zeros=0;
-	while(1) {		
-		if (temp % 10==0) {
-			temp = temp / 10;
-			zeros++;
-		}
-		else {
-			break;
-		}
-	}
-	return zeros;
-}
